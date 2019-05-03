3dc9790f25fa8517b74f1abb21c8c5a4104a2c1d
Jan Biasi <biasijan@gmail.com>
2019-05-03 18:08:22 +0200

    feat(module): add Configuration export for custom defaults, all named exports for future

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { Configuration } from "./Configuration";
 import { SDK } from "./SDK";
+
+export {
+  // export config for re-setting defaults across all SDK instances
+  Configuration,
+  // named exports is preferred, keep default for transition phase
+  SDK
+};
 
 export default SDK;
